ed637eba7fb778429815658444effe2d84ef9556
Dylan <dylflanagan@gmail.com>
2022-11-01 18:02:36 -0700

    Route built and tested for /zodiac

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,23 +3,12 @@ const app = require('../lib/app');
 
 const { signs } = require('../lib/zodiac-data.js');
 
-describe('zodiac routes', () => {
+describe('signs routes', () => {
   it('/zodiac should return list of signs', async () => {
     const res = await request(app).get('/zodiac');
     const expected = signs.map((sign) => {
       return { id: sign.id, name: sign.name };
+    });
+    expect(res.body).toEqual(expected);
   });
-  expected(res.body).toEqual(expected);
 });
-
-// it('/zodiac/:id should return cat detail', async () => {
-//   const res = await request(app).get('/zodiac/1');
-//   const felix = {
-//     id: '1',
-//     name: 'aquarius',
-//     dates: 'Jan 21 - Feb 19',
-//     symbol: 'Water Bearer',
-//   };
-//   expect(res.body).toEqual(felix);
-// });
-;
